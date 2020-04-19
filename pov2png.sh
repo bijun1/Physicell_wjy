2c05843e1a7a6d70e06635bbf4e28404e5a11d1f
Your Name <you@example.com>
2020-04-19 16:21:33 +0800

    fix(wjy) : pov

--- pov2png.sh
+++ pov2png.sh
@@ -1,4 +1,19 @@
-filenames=$(ls *.pov)
+if [ ! -d "/pov_file/" ];then
+mkdir /pov_file
+fi
+
+if [ ! -d "/png_file/" ];then
+mkdir /png_file
+fi
+
+mv *.pov pov_file
+
+filenames=$(ls pov_file/*.pov)
+res=2000;
 for f in ${filenames};do
-	~/Downloads/povray-3.6/povray -H2160 -W2160 ${f}
+	~/Downloads/povray-3.6/povray -H${res} -W${res} ${f}
 done
+
+mv *.png png_file/
+
+mv pov_file pov_file_done
